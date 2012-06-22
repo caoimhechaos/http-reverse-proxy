07d35ed6c11b3390ffc80b434445b8bd3ee67961
Tonnerre LOMBARD <tonnerre@ancient-solutions.com>
2012-06-22 02:29:41 +0200

    Add proper access.log support and a few expvars to track the status of the service. Stop logging the request time for now.
    Add a local web server port to access the debug vars.

--- backend_connection.go
+++ backend_connection.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"expvar"
 	"io"
 	"log"
 	"net"
@@ -10,8 +11,25 @@ import (
 	"time"
 )
 
+var ReconnectsPerBackend *expvar.Map =
+	expvar.NewMap("num-reconnects-per-backend")
+var ReconnectFailuresByReason *expvar.Map =
+	expvar.NewMap("num-reconnect-failures-by-reason")
+var RequestTimeSumPerBackend *expvar.Map =
+	expvar.NewMap("request-time-total-per-backend")
+
+func AccessLogRequest(accessLog *log.Logger, req *http.Request,
+	statusCode int, contentLength int64, begin time.Time) {
+	accessLog.Print(req.Host, " ", req.RemoteAddr, " - - [",
+		begin.UTC().Format("02/01/2006:15:04:05 -0700"), "] ",
+		strconv.Quote(req.Method+" "+req.RequestURI+" "+req.Proto),
+		" ", statusCode, contentLength, " ",
+		strconv.Quote(req.Referer()), " ", strconv.Quote(req.UserAgent()))
+}
+
 type BackendConnection struct {
 	dest                 string
+	log					 *log.Logger
 	clientConn           *httputil.ClientConn
 	tcpConn              net.Conn
 	weightedResponseTime time.Duration
@@ -19,9 +37,11 @@ type BackendConnection struct {
 	ready                bool
 }
 
-func NewBackendConnection(dest string) *BackendConnection {
+func NewBackendConnection(dest string,
+	logDest *log.Logger) *BackendConnection {
 	var be = &BackendConnection{
 		dest: dest,
+		log: logDest,
 	}
 	go be.CheckAndReconnect(nil)
 	return be
@@ -38,10 +58,12 @@ func (be *BackendConnection) CheckAndReconnect(e error) {
 		return
 	}
 	be.ready = false
+	ReconnectsPerBackend.Add(be.dest, 1)
 	be.tcpConn, err = net.DialTimeout("tcp", be.dest,
 		(2<<be.connectionAttempt)*time.Second)
 	if err != nil {
 		log.Print("Failed to connect to ", be.dest, ": ", err)
+		ReconnectFailuresByReason.Add(err.Error(), 1)
 		be.connectionAttempt = be.connectionAttempt + 1
 		time.Sleep((2 << be.connectionAttempt) * 100 * time.Millisecond)
 		go be.CheckAndReconnect(nil)
@@ -84,12 +106,7 @@ func (be *BackendConnection) Do(req *http.Request, w http.ResponseWriter) error 
 		}
 	}
 
-	log.Print("Request to ", req.URL.String(), " took ", passed)
-	log.Print(req.Host, " ", req.RemoteAddr, " - - [",
-		begin.UTC().Format("02/01/2006:15:04:05 -0700"), "] ",
-		strconv.Quote(req.Method+" "+req.RequestURI+" "+req.Proto),
-		" ", res.StatusCode, res.ContentLength, " ",
-		strconv.Quote(req.Referer()), " ", strconv.Quote(req.UserAgent()))
+	AccessLogRequest(be.log, req, res.StatusCode, res.ContentLength, begin) 
 
 	for key, values := range res.Header {
 		for _, value := range values {
@@ -102,6 +119,7 @@ func (be *BackendConnection) Do(req *http.Request, w http.ResponseWriter) error 
 	if err != nil {
 		return err
 	}
+	RequestTimeSumPerBackend.AddFloat(be.dest, passed.Seconds())
 
 	res.Body.Close()
 	return nil
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"code.google.com/p/goprotobuf/proto"
+	"expvar"
 	"flag"
 	"fmt"
 	"io"
@@ -11,6 +12,7 @@ import (
 	"net/http"
 	"os"
 	"sync"
+	"time"
 )
 
 type ReqHandler struct {
@@ -32,21 +34,41 @@ func (this *TargetsSpec) GetNextConnection() *BackendConnection {
 	return this.Backends[this.lru]
 }
 
+var requestsTotal *expvar.Int
+var requestsPerHost *expvar.Map
+var requestsPerBackend *expvar.Map
+var requestErrorsPerHost *expvar.Map
+var requestErrorsPerBackend *expvar.Map
+var requestErrorsPerError *expvar.Map
+
+var accessLog *log.Logger
+
 // Default Request Handler
 func (this *ReqHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	var targets *TargetsSpec = this.BackendMap[r.Host]
+	var host string;
 	var be, initbe *BackendConnection
+	var err error
 
-	if targets == nil {
-		host, _, err := net.SplitHostPort(r.Host)
-		if err == nil {
-			targets = this.BackendMap[host]
-		}
+	host, _, err = net.SplitHostPort(r.Host)
+	if err != nil {
+		host = r.Host
 	}
 
 	if targets == nil {
-		http.Error(w, "Host not configured", http.StatusServiceUnavailable)
+		targets = this.BackendMap[host]
+	}
+
+	requestsTotal.Add(1)
+	requestsPerHost.Add(host, 1)
+
+	if targets == nil {
+		http.Error(w, "Host not configured",
+			http.StatusServiceUnavailable)
 		log.Print("Received request for unknown host " + r.Host)
+		requestErrorsPerError.Add("unknown-host", 1)
+		AccessLogRequest(accessLog, r, http.StatusServiceUnavailable, -1,
+			time.Now())
 		return
 	}
 
@@ -55,18 +77,33 @@ func (this *ReqHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	r.Header.Del("Connection")
 
 	// We need to add an XFF header however.
-	r.Header.Add('X-Forwarded-For', r.RemoteAddr)
+	r.Header.Add("X-Forwarded-For", r.RemoteAddr)
 
 	initbe = targets.GetNextConnection()
+	if initbe == nil {
+		http.Error(w, "Backends not available",
+			http.StatusServiceUnavailable)
+		log.Print("Received request for " + r.Host +
+			" but all backends are down")
+		requestErrorsPerHost.Add(host, 1)
+		requestErrorsPerError.Add("no-backends", 1)
+		AccessLogRequest(accessLog, r, http.StatusServiceUnavailable, -1,
+			time.Now())
+		return
+	}
 	be = initbe
 	for {
-		err := be.Do(r, w)
+		requestsPerBackend.Add(be.String(), 1)
+		err = be.Do(r, w)
 
 		if err == nil {
 			return
 		} else {
 			log.Print("Error sending request to backend",
 				be.String(), ": ", err.Error())
+			requestErrorsPerHost.Add(host, 1)
+			requestErrorsPerError.Add(err.Error(), 1)
+			requestErrorsPerBackend.Add(be.String(), 1)
 			go be.CheckAndReconnect(err)
 		}
 
@@ -89,6 +126,7 @@ func main() {
 	var filename = flag.String("config", "", "Path to configuration file")
 	var config = new(ReverseProxyConfig)
 	var backendmap = make(map[string]*TargetsSpec)
+	var accessLogFile *os.File
 	var conffile io.Reader
 	var wg sync.WaitGroup
 	var data []byte
@@ -111,6 +149,32 @@ func main() {
 		log.Fatal(err)
 	}
 
+	if config.InfoServer != nil {
+		go func(addr string) {
+			err = http.ListenAndServe(addr, nil)
+			if err != nil {
+				log.Fatal("Unable to start info server on ", addr,
+					": ", err)
+			}
+		}(*config.InfoServer)
+	}
+	
+	requestsTotal = expvar.NewInt("requests-total")
+	requestsPerHost = expvar.NewMap("requests-per-host")
+	requestsPerBackend = expvar.NewMap("requests-per-backend")
+	requestErrorsPerHost = expvar.NewMap("request-errors-per-host")
+	requestErrorsPerBackend = expvar.NewMap("request-errors-per-backend")
+	requestErrorsPerError = expvar.NewMap("request-errors-per-error-type")
+
+	accessLogFile, err = os.OpenFile(*config.AccessLogPath,
+		os.O_WRONLY | os.O_APPEND | os.O_SYNC | os.O_CREATE, 0600)
+	if err != nil {
+		log.Fatal("Unable to open ", *config.AccessLogPath, " for writing: ",
+			err)
+	}
+
+	accessLog = log.New(accessLogFile, "", 0)
+
 	for _, target := range config.TargetConfig {
 		var spec *TargetsSpec = new(TargetsSpec)
 		var be_list []*BackendConnection
@@ -118,7 +182,8 @@ func main() {
 		for _, backend := range target.Be {
 			var dest string = net.JoinHostPort(*backend.Host,
 				fmt.Sprintf("%d", *backend.Port))
-			var conn *BackendConnection = NewBackendConnection(dest)
+			var conn *BackendConnection =
+				NewBackendConnection(dest, accessLog)
 
 			if err != nil {
 				log.Printf("Unable to connect to %s: %s",
@@ -149,7 +214,8 @@ func main() {
 			srv.Handler = handler
 
 			if p.SslCertPath != nil && p.SslKeyPath != nil {
-				err = srv.ListenAndServeTLS(*p.SslCertPath, *p.SslKeyPath)
+				err = srv.ListenAndServeTLS(*p.SslCertPath,
+					*p.SslKeyPath)
 			} else {
 				err = srv.ListenAndServe()
 			}
